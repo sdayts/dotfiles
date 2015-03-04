a52a105897dc6a62f7f5cdb5ee300c13281f9455
Sergey Dayts <sdayts@gmail.com>
2015-03-03 21:27:37 -0500

    Added new hooks

---
+++ emacs.d/sd/hooks/ace-jump.el
@@ -0,0 +1,6 @@
+(setq ace-jump-mode-scope 'window)
+(setq ace-jump-mode-gray-background nil)
+
+(custom-set-faces
+ '(ace-jump-face-foreground
+   ((t (:inherit ace-jump-face-foreground :height  :foreground "yellow" :background "black" )))))
