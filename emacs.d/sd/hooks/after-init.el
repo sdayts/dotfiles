84e6271ce5e370fb36b450b923ee70ad15805a2b
Sergey Dayts <sdayts@gmail.com>
2015-03-10 22:01:12 -0400

    Latest improvements.

--- emacs.d/sd/hooks/after-init.el
+++ emacs.d/sd/hooks/after-init.el
@@ -124,7 +124,29 @@
       '("^\\*shell\\*$" . (display-buffer-same-window)))
 
 ;; Expand region
+(global-unset-key (kbd "C-="))
 (global-set-key (kbd "C-=") 'er/expand-region)
+
+;; Kick off dired with Ctrl-l
+(global-unset-key (kbd "C-l"))
+(global-set-key (kbd "C-l") 
+		(lambda ()
+		  (interactive)
+		  (dired ".") ))
+
+;; Backup file configuration
+(setq backup-directory-alist `(("." . "~/.emacs.d/backups")))
+(setq make-backup-files t               ; backup of a file the first time it is saved.
+      backup-by-copying t               ; don't clobber symlinks
+      version-control t                 ; version numbers for backup files
+      delete-old-versions t             ; delete excess backup files silently
+      delete-by-moving-to-trash t
+      kept-old-versions 6               ; oldest versions to keep when a new numbered backup is made (default: 2)
+      kept-new-versions 9               ; newest versions to keep when a new numbered backup is made (default: 2)
+      auto-save-default t               ; auto-save every buffer that visits a file
+      auto-save-timeout 20              ; number of seconds idle time before auto-save (default: 30)
+      auto-save-interval 200            ; number of keystrokes between auto-saves (default: 300)
+      )
 
 ;; helm projectile
 ;; TODO move to its own mode file
@@ -175,10 +197,14 @@
 (set-face-attribute 'region nil :background "black" :foreground "yellow" )
 (set-face-attribute 'isearch nil :background "black" :foreground "yellow" )
 (set-face-attribute 'lazy-highlight nil :background "black" :foreground "cyan" )
-
+;; mode line colors
 (set-face-attribute 'mode-line nil :background "black" :foreground "yellow" )
 (set-face-attribute 'mode-line-inactive nil :background "dimgray" :foreground "white" )
 ;; (set-face-attribute 'header-line nil :background "black" :foreground "yellow" )
+
+;; hilight current line
+(global-hl-line-mode +1)
+(set-face-background hl-line-face "#005A64")
 
 ;; Kick off required modes
 (helm-mode 1)
