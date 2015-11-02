8224ba45e2bbf715f23d4d9fb1cb584a585da453
Sergey Dayts <sdayts@gmail.com>
2015-11-01 21:17:10 -0500

    Latest changes.

--- emacs_tap/sd/hooks/c++-mode.el
+++ emacs_tap/sd/hooks/c++-mode.el
@@ -1,4 +1,11 @@
+;; window movement
 (local-unset-key (kbd "M-j"))
 (local-unset-key (kbd "M-<down>"))
 (local-set-key (kbd "M-j") 'windmove-down)
 (local-set-key (kbd "M-<down>") 'windmove-down)
+
+;; Comment bindings
+(local-unset-key (kbd "C-c C-c"))
+(local-set-key (kbd "C-c C-c") 'sd/comment-line-or-region)
+(local-unset-key (kbd "C-c C-u"))
+(local-set-key (kbd "C-c C-u") 'sd/comment-line-or-region)
