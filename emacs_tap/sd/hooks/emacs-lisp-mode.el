fab4a0b97194f68eb3b9e3ef7ba53fc139694469
Sergey Dayts <sdayts@gmail.com>
2015-11-21 14:22:08 -0500

    New hook for emacs lisp mode.

---
+++ emacs_tap/sd/hooks/emacs-lisp-mode.el
@@ -0,0 +1,5 @@
+;; Comment bindings
+(local-unset-key (kbd "C-c C-c"))
+(local-set-key (kbd "C-c C-c") 'sd/comment-line-or-region)
+(local-unset-key (kbd "C-c C-u"))
+(local-set-key (kbd "C-c C-u") 'sd/comment-line-or-region)
